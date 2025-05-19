e9ac7f8244f8ccb73f22beec21bcec058ae59153
Olli Hilke <hilkolli@student.uef.fi>
2025-05-19 13:19:10 +0300

    alun määrittelyjä lisää. Kielellistys i18n kirjastolla alustettu

--- src/components/testComponent.tsx
+++ src/components/testComponent.tsx
@@ -1,19 +1,25 @@
-import { Button } from "@knicos/genai-base"
-import { useState } from "react"
+import { Button } from '@knicos/genai-base';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function ButtonTest() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const { t } = useTranslation();
 
-  const handleSetCount = () => {
-    setCount(count + 1)
-    console.log(count)
-  }
+    const handleSetCount = () => {
+        setCount(count + 1);
+        console.log(count);
+    };
 
-  return (
-    <Button onClick={() => handleSetCount()} size="large" variant="contained">
-      testi
-    </Button>
-  )
+    return (
+        <Button
+            onClick={() => handleSetCount()}
+            size="large"
+            variant="contained"
+        >
+            {t('test.button')}
+        </Button>
+    );
 }
 
-export default ButtonTest
+export default ButtonTest;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './i18n';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import { theme } from '@knicos/genai-base';

928265199ef9f2f3993d63a8932594a74958ff10
Olli Hilke <hilkolli@student.uef.fi>
2025-05-20 15:34:59 +0300

    kuvan otto toteutettu

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -1,3 +1,5 @@
 import { atom } from 'jotai';
 
 export const testAtom = atom(0);
+
+export const imageAtom = atom<HTMLCanvasElement | null>(null);
--- src/views/Game/Own/OwnGame.tsx
+++ src/views/Game/Own/OwnGame.tsx
@@ -2,9 +2,33 @@ import style from './style.module.css';
 import { useTranslation } from 'react-i18next';
 import Header from '../../../components/Header/Header';
 import Footer from '../../../components/Footer/Footer';
+import { Button, Webcam } from '@knicos/genai-base';
+import { useCallback, useState } from 'react';
+import { useAtom } from 'jotai';
+import { imageAtom } from '../../../utils/state';
 
 export default function OwnGame() {
     const { t } = useTranslation();
+    const [image, setImage] = useAtom(imageAtom);
+    const [hasCaptured, setHasCaptured] = useState(false);
+    const [capture, setCapture] = useState(false);
+
+    const getImage = useCallback(
+        (img: HTMLCanvasElement) => {
+            setImage(img);
+            setHasCaptured(true);
+        },
+        [setImage]
+    );
+
+    const resetCapture = () => {
+        setImage(null);
+        setHasCaptured(false);
+        setCapture(false);
+    };
+
+    const startCapture = useCallback(() => setCapture(true), [setCapture]);
+    const stopCapture = useCallback(() => setCapture(false), [setCapture]);
 
     return (
         <div className={style.container}>
@@ -13,7 +37,38 @@ export default function OwnGame() {
                 <div className={style.innerContainer}>
                     <h1>{t('common.title')}</h1>
                     <h2>{t('game.own.title')}</h2>
-                    {/**Tähän tulee jotain alkuasetteluja */}
+                    {!hasCaptured && (
+                        <>
+                            <Webcam
+                                size={224}
+                                interval={1000}
+                                onCapture={getImage}
+                                capture={capture}
+                            />
+                            <Button
+                                onMouseDown={startCapture}
+                                onMouseUp={stopCapture}
+                                onBlur={stopCapture}
+                                onMouseLeave={stopCapture}
+                                onTouchEnd={stopCapture}
+                                onTouchCancel={stopCapture}
+                            >
+                                ota kuva
+                            </Button>
+                        </>
+                    )}
+
+                    {hasCaptured && image && (
+                        <>
+                            <img
+                                src={image.toDataURL()}
+                                alt="Kuvakaappaus"
+                                style={{ width: '224px', height: '224px' }}
+                            />
+                            <Button onClick={resetCapture}>{t('game.own.takeNew', 'Ota uusi kuva')}</Button>
+                        </>
+                    )}
+                    {/**Tähän tulee jotain alkuasetteluja kuten kuvan lataus*/}
                     {/**Tähän tulee valmis pelin aloituspainike, jonka painalluksen jälkeen peli alkaa */}
                     {/*&&ready niin renderöityy kuva, johon sitten */}
                 </div>
